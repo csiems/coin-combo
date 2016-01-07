ad4f09b4e82125f0edf08746c893237950766b74
Chris Siems and Midori Bowen <csiems@gmail.com>
2016-01-07 09:34:57 -0800

    Pass first test.

--- coin-combo/src/main/java/App.java
+++ coin-combo/src/main/java/App.java
@@ -9,5 +9,15 @@ public class App {
     //Spark and Velocity go in here!
   }
 
-  
+  public static Integer howMuchChange (Integer userInput) {
+    Integer quarters = 0;
+
+    while (userInput > 24) {
+      userInput -= 25;
+      quarters++;
+    }
+
+    return quarters;
+    // String.format("You receive %d quarters, %d dimes, %d nickels, and %d pennies", quarters, dimes, nickels, pennies);
+  }
 }
--- coin-combo/src/test/java/AppUnitTest.java
+++ coin-combo/src/test/java/AppUnitTest.java
@@ -1,8 +1,13 @@
-import org.junit.*
-import static org.junit.Assert.*
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class AppUnitTest {
 
-
+@Test
+public void howMuchChange_forQuartersReceived_three() {
+  App appTest = new App();
+  Integer quarters = 3;
+  assertEquals(quarters, appTest.howMuchChange(75));
+}
 
 }
