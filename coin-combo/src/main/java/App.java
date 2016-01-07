a90c82ccdb7e23327e160ca6914acc8c7a1318d0
Chris Siems and Midori Bowen <midori.p.bowen@gmail.com>
2016-01-07 10:32:13 -0800

    Pass the nickel test.

--- coin-combo/src/main/java/App.java
+++ coin-combo/src/main/java/App.java
@@ -1,23 +1,45 @@
+import java.io.Console;
+import java.lang.*;
 import java.util.Map;
 import java.util.HashMap;
-import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-import static spark.Spark.*;
+//import spark.ModelAndView;
+//import spark.template.velocity.VelocityTemplateEngine;
+//import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    //Spark and Velocity go in here!
+    Console myConsole = System.console();
+    System.out.println("How many cents?");
+    String userInput = myConsole.readLine();
+    String output = howMuchChange(userInput);
+    System.out.println(output);
   }
 
-  public static Integer howMuchChange (Integer userInput) {
+  public static String howMuchChange (String userInput) {
     Integer quarters = 0;
+    Integer dimes = 0;
+    Integer nickels = 0;
+    Integer pennies = 0;
 
-    while (userInput > 24) {
-      userInput -= 25;
+    Integer userInputAsInteger = Integer.parseInt(userInput);
+
+    while (userInputAsInteger > 24) {
+      userInputAsInteger -= 25;
       quarters++;
     }
 
-    return quarters;
-    // String.format("You receive %d quarters, %d dimes, %d nickels, and %d pennies", quarters, dimes, nickels, pennies);
+    while (userInputAsInteger > 9) {
+      userInputAsInteger -= 10;
+      dimes++;
+    }
+
+    while (userInputAsInteger > 4) {
+      userInputAsInteger -= 5;
+      nickels++;
+    }
+
+    String output = String.format("You receive %d quarters, %d dimes, %d nickels, and %d pennies", quarters, dimes, nickels, pennies);
+    return output;
+
   }
 }
--- coin-combo/src/test/java/AppUnitTest.java
+++ coin-combo/src/test/java/AppUnitTest.java
@@ -6,8 +6,19 @@ public class AppUnitTest {
 @Test
 public void howMuchChange_forQuartersReceived_three() {
   App appTest = new App();
-  Integer quarters = 3;
-  assertEquals(quarters, appTest.howMuchChange(75));
+  assertEquals("You receive 3 quarters, 0 dimes, 0 nickels, and 0 pennies", appTest.howMuchChange(75));
+}
+
+@Test
+public void howMuchChange_forDimesReceived_threeQuartersTwoDimes() {
+  App appTest = new App();
+  assertEquals("You receive 3 quarters, 2 dimes, 0 nickels, and 0 pennies", appTest.howMuchChange(95));
+}
+
+@Test
+public void howMuchChange_forNickelReceived_oneQuarterOneDimeOneNickel() {
+  App appTest = new App();
+  assertEquals("You receive 1 quarters, 1 dimes, 1 nickels, and 0 pennies", appTest.howMuchChange(40));
 }
 
 }
